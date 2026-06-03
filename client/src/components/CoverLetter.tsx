21c4a640fa09d05a35c4a16bbccf0111c1c982ce
Palak-7 <palaksehgal0707@gmail.com>
2026-06-03 21:57:08 +0530

    use auth bugs

--- client/src/components/CoverLetter.tsx
+++ client/src/components/CoverLetter.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import api from '../services/api'
-import { useAuth } from '../context/AuthContext'
+import { useAuth } from '../context/useAuth'
 
 interface Props {
     resumeId?: string
