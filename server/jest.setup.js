4e08e8c83236836721da37d2bc9b2dbcc175fcf7
Palak-7 <palaksehgal0707@gmail.com>
2026-06-09 07:57:53 +0530

    error: fixed test bugs

--- server/jest.setup.js
+++ server/jest.setup.js
@@ -1,4 +1,7 @@
 process.env.NODE_ENV = "test";
+process.env.JWT_SECRET = process.env.JWT_SECRET || "test_secret_key";
+process.env.GROQ_API_KEY = process.env.GROQ_API_KEY || "test_groq_key";
+process.env.RESEND_API_KEY = process.env.RESEND_API_KEY || "test_resend_key";
 
 jest.mock("pdf-parse", () =>
   jest.fn().mockResolvedValue({
